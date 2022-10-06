4d9f1cb452a27a531e4ed29a627620688e3c5a0b
OmarRassem <Omar.rassem00@gmail.com>
2022-10-07 00:10:07 +0800

    add database model

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
-from starlette.responses import FileResponse 
+from tortoise.contrib.pydantic import pydantic_model_creator
+from starlette.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
+from passlib.hash import bcrypt
+from tortoise import fields
+from tortoise.models import Model
+from tortoise.contrib.fastapi import register_tortoise
 import uvicorn
 
 
@@ -11,23 +16,50 @@ app.mount("/assets", StaticFiles(directory="assets"), name="assets")
 
 @app.get("/")
 async def read_index():
-    return FileResponse('assets/index.html')
+    return FileResponse("assets/index.html")
+
 
 @app.get("/whoami")
 async def auth():
 
-    return {"token" : "Welcome"}
+    return {"token": "Welcome"}
+
 
 @app.get("/login")
 async def read_index():
-    return {"HI" : "U"}
+    return {"HI": "U"}
 
-""" @app.get("/user_home")
-async def read_index():
-    return FileResponse('assets/user_home.html') """
 
+# Model
+class user(Model):
+    id = fields.IntField(pk = True)
+    username = fields.CharField(50, unique=True)
+    password_hash = fields.CharField(128)
+
+    @classmethod
+    async def get_user(cls, username):
+        return cls.get(username = username)
+
+    def verify_password(self, password):
+        return bcrypt.verify(password, self.password_hash)
+
+user_pydantic = pydantic_model_creator(user, name='User')
+userIn_pydantic = pydantic_model_creator(user, name='UserIn', exclude_readonly= True)
+
+@app.post('/users', response_model= user_pydantic)
+async def create_user(users: userIn_pydantic):
+    user_obj = user(username= users.username, password_hash = bcrypt.hash(users.password_hash))
+    await user_obj.save()
+    return await user_pydantic.from_tortoise_orm(user_obj)
+
+register_tortoise(
+    app,
+    db_url="sqlite://db.sqlite",
+    modules={"modules": ["main"]},
+    generate_schemas=True,
+    add_exception_handlers=True,
+)
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host = "127.0.0.1", reload=True)
-
+    uvicorn.run("main:app", host="127.0.0.1", reload=True)
